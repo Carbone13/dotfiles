3a5d7e126bc53f7565b0da2703e7652df5926b07
Lucas Michaudel <lucas.michaudel@gmail.com>
2024-02-13 18:38:54 +0100

    Update brewer.sh

--- scripts/brewer.sh
+++ scripts/brewer.sh
@@ -6,8 +6,8 @@
 brew update
 brew upgrade
 
-brew bundle install --file=../packages.brew
-brew bundle cleanup --force --zap --file=../packages.brew
+brew bundle install --file=packages.brew
+brew bundle cleanup --force --zap --file=packages.brew
 brew cu
 
 echo "Brew packages synced and up-to-date."
