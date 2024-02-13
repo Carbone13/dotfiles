6980a63dc4ee23322aaa8f183c13b9a500810431
Lucas Michaudel <lucas.michaudel@gmail.com>
2024-02-13 18:24:36 +0100

    Update prelude.sh

--- scripts/prelude.sh
+++ scripts/prelude.sh
@@ -7,7 +7,6 @@ command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; \
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
 brew analytics off
 
-(echo; echo 'eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"') >> ${shell_rcfile}
-eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+eval "/opt/homebrew/bin/brew shellenv)"
 
 echo "Prelude done."
